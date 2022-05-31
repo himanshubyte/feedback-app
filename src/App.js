3409c4298a2afe82409be531c927b747a0146dc3
Himanshu <pandey.himanshu@walwil.com>
2022-05-31 12:37:01 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,33 +1,12 @@
-import './App.css';
+import "./App.css";
 
-const App = ()=>{
-  const title="Blog Post";
-  const body='This is my blog post';
-
-  const comments= [
-    {id:1 , text:"Comment one"},
-    {id:2 , text:"Comment two"},
-    {id:3 , text:"Comment three "},
-
-  ]
-
+const App = () => {
   return (
-      <div className="container">
-        <h1>{title.toUpperCase()}</h1>
-        <p>{body}</p>
-        <div className="comments">
-          <h3>Comments ({comments.length})</h3>
-
-          <ul>
-              {comments.map((comment,index)=>(
-                <li key={index} > {comment.text} </li>
-              ))}
-          </ul>
-        </div>
-      </div>
-     
-
-  )
-}
+    <div className="one">
+      <h1>My App</h1>
+      <p>This is a test App</p>
+    </div>
+  );
+};
 
 export default App;
