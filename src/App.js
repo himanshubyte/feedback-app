c05a906e822ce7d720cb34670a70fa9e0cd7601d
Himanshu <pandey.himanshu@walwil.com>
2022-05-31 13:22:05 +0530

    dynamic values and map use

--- src/App.js
+++ src/App.js
@@ -4,10 +4,27 @@ const App = () => {
 const title="Blog Post"
 const body="This is my blog post"
 
+const comments=[
+  {id:1, text:'Comment one'},
+  {id:2, text:'Comment two'},
+  {id:3, text:'Comment three'}
+]
+//map returns jsx 
+//!comment.map(()=>())
+
   return (
-    <div className="one">
-      <h1>{title}</h1>
+    <div className="container">
+      <h1>{title.toUpperCase()}</h1>
       <p>{body}</p>
+      
+      <div className="comments">
+        <h4>Comments({comments.length })</h4>
+        <ul>
+        {comments.map((comment,index)=>(
+            <li key={index} >{comment.text}</li>
+        ))}
+        </ul>
+      </div>
     </div>
   );
 };
